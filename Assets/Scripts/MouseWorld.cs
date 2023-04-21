9f81432e0884652281b6ba969cb2673f329b8133
JVdeOliveira <joaooliveira.jvo@outlook.com>
2023-04-21 00:52:22 -0300

    Mouse World
    - MouseWold static class

---
+++ Assets/Scripts/MouseWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseWorld : MonoBehaviour
+{
+    public static Vector3 Position
+    {
+        get
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (Physics.Raycast(ray, out RaycastHit hitInfo, Camera.main.farClipPlane))
+            {
+                return hitInfo.point;
+            }
+            return Vector3.zero;
+        }
+    }
+}
